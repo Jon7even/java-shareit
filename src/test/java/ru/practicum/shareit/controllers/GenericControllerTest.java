583658550d46fa3ae3641f708460418a6caa1dae
JonSeven <mazahaker5555@yandex.ru>
2023-07-02 00:02:51 +0300

    style: delete import

--- src/test/java/ru/practicum/shareit/controllers/GenericControllerTest.java
+++ src/test/java/ru/practicum/shareit/controllers/GenericControllerTest.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
